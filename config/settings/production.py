26ce260a438196c9d3e4606a75e4ebe18833d1d4
Daniel Fernandes <daniel.dourado@gmail.com>
2017-12-08 12:21:13 +0000

    Changed app type to python363

--- config/settings/production.py
+++ config/settings/production.py
@@ -21,7 +21,7 @@ from .base import *  # noqa
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'SOME+RANDOM+KEY(z9+3vnm(jb0u@&w68t#5_e8s9-lbfhv-')  
 
 
 # This ensures that Django will be able to detect a secure connection
