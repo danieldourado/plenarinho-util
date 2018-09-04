d99455f3c355bee8bd4e030c36c4a7b72faa1891
Daniel Fernandes <daniel.dourado@gmail.com>
2018-09-04 16:30:58 +0000

    commit before deleting team workspace

--- projetos/urls.py
+++ projetos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ProjetoList.as_view(), name='list'),
+    url(r'^list_for_pdf/$', views.ProjetoListForPDF.as_view(), name='list_for_pdf'),
     url(r'^new/$', views.ProjetoCreate.as_view(), name='create'),
     url(r'^extract/$', views.ProjetoCrawl, name='extract'),
     url(r'^(?P<pk>\d+)/$', views.ProjetoDetail.as_view(), name='detail'),
